caa953dd4d1f7628a2e2ffee63bd1922eb97a73d
tarun13rathore <tarun13rathore@gmail.com>
2023-12-03 12:41:03 +0530

    fix

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -3,8 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // Load balance from localStorage if available
 const loadBalanceFromLocalStorage = () => {
-  const storedBalance = localStorage?.getItem("accountBalance");
-  return storedBalance ? parseFloat(storedBalance) : 0;
+  try {
+    const storedBalance = localStorage?.getItem("accountBalance");
+    return storedBalance ? parseFloat(storedBalance) : 0;
+  } catch (error) {
+    console.error("Error accessing localStorage:", error);
+    return 0;
+  }
 };
 
 const accountSlice = createSlice({
@@ -31,27 +36,3 @@ const accountSlice = createSlice({
 export const { setBalance, addAmount, withdrawAmount } = accountSlice.actions;
 export const selectBalance = (state) => state.account.balance;
 export default accountSlice.reducer;
-
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const accountSlice = createSlice({
-//   name: "account",
-//   initialState: {
-//     balance: 0,
-//   },
-//   reducers: {
-//     setBalance: (state, action) => {
-//       state.balance = action.payload;
-//     },
-//     addAmount: (state, action) => {
-//       state.balance += action.payload;
-//     },
-//     withdrawAmount: (state, action) => {
-//       state.balance -= action.payload;
-//     },
-//   },
-// });
-
-// export const { setBalance, addAmount, withdrawAmount } = accountSlice.actions;
-// export const selectBalance = (state) => state.account.balance;
-// export default accountSlice.reducer;
